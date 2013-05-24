1b2f608b36e2d6b91fb8d831cde69523758d2a9f
Chris Waltrip <walt2178@vandals.uidaho.edu>
2013-05-24 12:32:55 -0700

    Modified good_sequences.bash to also handle reversing the sequences if necessary

--- bin/bash/good_sequences.bash
+++ bin/bash/good_sequences.bash
@@ -4,8 +4,9 @@
 #	1.  The directory with the sequences
 #	2.  The perl script directory
 #	3.  The file to store the list of input sequences for processing
-#	4.  The file to store the list of good sequences
+#	4.  The directory for blast files
 #	5.  The fasta file file extension
+#	6.  The direction of the sequences
 #
 # Returns 0 on success
 ###########
@@ -13,8 +14,12 @@
 SEQUENCE_DIR="$1"
 PERL_DIR="$2"
 INPUT_SEQUENCES_FILE="$3"
-GOOD_SEQUENCES_FILE="$4"
-$SUFFIX="$5"
+BLAST_DIR="$4"
+SUFFIX="$5"
+DIRECTION="$6"
+
+GOOD_SEQUENCES_FILE="${BLAST_DIR}good_sequences"
+
 
 echo "Collating sequences..."
 find ${SEQEUNCE_DIR} -maxdepth 1 -name "*${SUFFIX}" -print0 | xargs -i -0 cat {} >> ${INPUT_SEQUENCES_FILE}
@@ -33,6 +38,22 @@ then
 fi
 
 if [ ! -s ${GOOD_SEQEUNCE_FILE} ]
+then
+  echo "No good sequences found!  Exiting."
+	exit 1
+fi
+
+#If our direction is REVERSE, then we will reverse the sequences within
+#goodseqs and replace it with the reversed sequences
+case ${DIRECTION} in
+   REVERSE|reverse|R|r)
+     echo "Reverse orientation, reversing sequences"
+     seqret -srev -sequence ${GOOD_SEQUENCES_FILE} -offormat2 fasta -outseq revseqs -auto
+     mv ${GOOD_SEQUENCES_FILE} ${BLAST_DIR}beforereverse
+     mv ${BLASTDIR}revseqs ${GOOD_SEQUENCES_FILE}
+esac
+
+if [ ! -e ${GOOD_SEQUENCES_FILE} ]
 then
   echo "No good sequences found!  Exiting."
 	exit 1
