0f5725f60b84e4cf2abbbd21732d4974109f7005
Chris Waltrip <walt2178@vandals.uidaho.edu>
2013-07-17 15:50:19 -0700

    Added helper functions that the shell scripts call to generate filenames and help withh error checking.

--- scripts/pipeline_prep.sh
+++ scripts/pipeline_prep.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-
+# pipeline_prep.sh
+#
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This script copies the references file and blast sequences file
+# and places them in the backup directory.
+##########
 
-#####
-# Requires:
-#	REFERENCE_STRAINS: 			RefStrains File from config file
-#	BLAST_SEQUENCES: 			Blast Sequences from config file
-#	REFERENCES_DIR: 			Backup directory
-#####
-
-# Get helper functions for filenames
+# Grab the helper functions to get
+# generate the correct filenames for 
+# HiTSAPipe's error checking.
 source helpers
 
-# Get filenames
-SUCCESS_FILE=$(get_success_name)
-FAILURE_FILE=$(get_failure_name)
+SUCCESS_FILE=$(get_success)
+FAILURE_FILE=$(get_failure)
 
-
-echo "Copying the reference strains and blast sequences to backup directory."
-if [ ${DEBUG} == "True" ]
-	then
-		echo "cp ${REFERENCE_STRAINS} ${REFERENCES_DIR}/"
-		echo "cp ${BLAST_SEQUENCES} ${REFERENCES_DIR}/"
-fi
-
+echo "Copying the reference strains to backup directory..."
 cp ${REFERENCE_STRAINS} ${REFERENCES_DIR}/
+ERROR_MSG="Cannot copy reference strains file."
+exit_if_error
 
+echo "Copying the blast sequences to backup directory..."
 cp ${BLAST_SEQUENCES} ${REFERENCES_DIR}/
+ERROR_MSG="Cannot copy blast sequences file."
+exit_if_error
+
+# If everything went well, call exit
+exit_success
